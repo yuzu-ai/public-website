e2741bf4027add066528b4658f85967c7ac5d1e1
Sam Yu <yudataguy@gmail.com>
2023-06-24 16:11:51 +0900

    update style and blog

--- src/layouts/components/MDXContent.tsx
+++ src/layouts/components/MDXContent.tsx
@@ -1,10 +1,14 @@
 import shortcodes from "@/shortcodes/all";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import remarkGfm from "remark-gfm";
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math'
+import rehypeHighlight from 'rehype-highlight'
 
 const MDXContent = ({ content }: { content: any }) => {
   const mdxOptions = {
-    remarkPlugins: [remarkGfm],
+    remarkPlugins: [remarkGfm, remarkMath],
+    rehypePlugins: [rehypeKatex, rehypeHighlight],
   };
   return (
     <>
