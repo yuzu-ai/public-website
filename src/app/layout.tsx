441b74fd278bbc6f287cb4f9e9e42213087eacf6
Sam S. Yu <25761223+yudataguy@users.noreply.github.com>
2023-07-18 14:28:05 +0900

    update ga loc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,14 +64,6 @@ export default function RootLayout({
         </Providers>
       </body>
 
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-B3WBLPNZFH"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-B3WBLPNZFH');
-      </script>
     </html>
   );
 }
